d9e9257d22d1209ded4abb62bf386593c4149d3e
NasirNobin <nasir.nobin@gmail.com>
2023-11-30 23:49:57 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 if(! empty($_REQUEST['state'])){
-	header('Location: ' . $_REQUEST['state'] .'?'.  http_build_query($_REQUEST));
+    header('Location: ' . $_REQUEST['state'] .'?'.  http_build_query($_REQUEST));
 }else{
-	echo "Please visit <a href='https://yive.io'>yive.io</a>";
+    echo "Please visit <a href='https://yive.io'>yive.io</a>";
 }
